86767b794d9896e98595a605200f1bfb7c33838c
H A S A N <hasan25042019@gmail.com>
2023-07-18 12:39:30 +0600

    Update UserController.php
    dfd

--- Module-20/pos/app/Http/Controllers/UserController.php
+++ Module-20/pos/app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Helper\JWTToken;
 use Exception;
 
 class UserController extends Controller
@@ -30,5 +31,45 @@ class UserController extends Controller
             ]);
         }
         
+    }
+
+    function UserLogin(Request $request){                       //when user data match the database then login successful
+        $count = User::where('email', '=', $request->input('email'))
+        -> where('password', '=', $request->input('password'))
+        -> count();                                //if return count value = 1 that's mean completely match user data
+
+        if($count == 1){
+            $token = JWTToken::createToken($request->input('email'));    // we recive jwt token from JWTToken.php file
+            return response()->json([
+                'status' => 'success',
+                'message' => 'User Login Successful',
+                'token' => $token
+            ]);
+        }
+
+        else{
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'unauthorized'
+            ]);
+        }
+    }
+
+    function SendOTPCode(Request $request){
+        $email = $request->input('email');              // get user email
+        $otp = rand(1000, 9999);                        // generate random otp code
+
+        $count = User::where('email', '=', $email)->count();    // matching user email
+
+        if($count == 1){
+            //
+        }
+
+        else{
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'unauthorized'
+            ]);
+        }
     }
 }
