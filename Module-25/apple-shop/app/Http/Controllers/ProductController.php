aa0def577553e14f74429338fcc9ea33e13b3558
H A S A N <hasan25042019@gmail.com>
2023-09-03 22:38:32 +0600

    gtr

--- Module-25/apple-shop/app/Http/Controllers/ProductController.php
+++ Module-25/apple-shop/app/Http/Controllers/ProductController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Helper\ResponseHelper;
+use App\Models\CustomerProfile;
 use App\Models\ProductSlider;
 use App\Models\Product;
 use App\Models\ProductDetails;
 use App\Models\ProductReview;
+use App\Models\ProductWish;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -42,5 +44,48 @@ class ProductController extends Controller
             $query -> select('id', 'cus_name');
         }]) -> get();
         return ResponseHelper::Out('success', $data, 200);
+    }
+
+    public function CreateProductReview(Request $request): JsonResponse{
+        $user_id = $request -> header('id');
+        $profile = CustomerProfile::where('user_id', $user_id) -> first();
+
+        if($profile){
+            $request -> merge(['customer_id' => $profile -> id]);
+            $data = ProductReview::updateOrCreate(
+                ['customer_id' => $profile -> id, 'product_id' => $request -> input('product_id')],
+                $request -> input()
+            );
+            return ResponseHelper::Out('success', $data, 200);
+        }
+        else{
+            return ResponseHelper::Out('fail', 'Customer Profile Not Exists', 200);
+        }
+    }
+
+    public function ProductWishList(Request $request): JsonResponse{
+        $user_id = $request -> header('id');
+        $data = ProductWish::where('user_id', $user_id) -> with('product') -> get();
+        return ResponseHelper::Out('success', $data, 200);
+    }
+
+    public function CreateWishList(Request $request): JsonResponse{
+        $user_id = $request -> header('id');
+        $data = ProductWish::updateOrCreate(
+            ['user_id' => $user_id, 'product_id' => $request -> product_id],
+            ['user_id' => $user_id, 'product_id' => $request -> product_id],
+        );
+        return ResponseHelper::Out('success', $data, 200);
+    }
+
+    public function RemoveWishList(Request $request): JsonResponse{
+        $user_id = $request -> header('id');
+        $data = ProductWish::where(['user_id' => $user_id, 'product_id' => $request -> product_id]) -> delete();
+        return ResponseHelper::Out('success', $data, 200);
+    }
+
+    public function CreateCartList(Request $request): JsonResponse{
+        $user_id = $request -> header('id');
+        
     }
 }
