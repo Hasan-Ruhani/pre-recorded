a107669efd527fae1abfab620044ab8bfb8d9a9e
H A S A N <hasan25042019@gmail.com>
2023-09-09 20:07:42 +0600

    Update ProductController.php
    fdfhg

--- Module-25/apple-shop/app/Http/Controllers/ProductController.php
+++ Module-25/apple-shop/app/Http/Controllers/ProductController.php
@@ -2,130 +2,66 @@
 
 namespace App\Http\Controllers;
 
-use App\Helper\ResponseHelper;
 use App\Models\CustomerProfile;
-use App\Models\ProductSlider;
-use App\Models\Product;
+use App\Models\Invoice;
+use App\Models\InvoiceProduct;
 use App\Models\ProductCart;
-use App\Models\ProductDetails;
-use App\Models\ProductReview;
-use App\Models\ProductWish;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
-class ProductController extends Controller
+class InvoiceController extends Controller
 {
-    public function ListProductByCategory(Request $request): JsonResponse{
-        $data = Product::where('category_id', $request->id)->with('brand', 'category')->get();
-        return ResponseHelper::Out('success', $data, 200);
-    }
+    // public function PolicyType(Request $request){
+    //     return Policy::where('type', $request -> type)->first();
+    // }
 
-    public function ListProductByRemark(Request $request): JsonResponse{
-        $data = Product::where('remark', $request->remark)->with('brand', 'category')->get();
-        return ResponseHelper::Out('success', $data, 200);
-    }
-
-    public function ListProductByBrand(Request $request): JsonResponse{
-        $data = Product::where('brand_id', $request->id)->with('brand', 'category')->get();
-        return ResponseHelper::Out('success', $data, 200);
-    }
-
-    public function ListProductSlider(Request $request): JsonResponse{
-        $data = ProductSlider::all();
-        return ResponseHelper::Out('success', $data, 200);
-    }
-
-    public function ProductDetailsById(Request $request): JsonResponse{
-        $data = ProductDetails::where('product_id', $request->id)->with('product', 'product.brand', 'product.category')->get();
-        return ResponseHelper::Out('success', $data, 200);
-    }
-
-    public function ListReviewByProduct(Request $request): JsonResponse{
-        $data = ProductReview::where('product_id', $request->product_id)->with(['profile'=>function($query){
-            $query -> select('id', 'cus_name');
-        }]) -> get();
-        return ResponseHelper::Out('success', $data, 200);
-    }
-
-    public function CreateProductReview(Request $request): JsonResponse{
+    public function InvoiceCreate(Request $request): JsonResponse{
         $user_id = $request -> header('id');
-        $profile = CustomerProfile::where('user_id', $user_id) -> first();
+        $user_email = $request -> header('email');
 
-        if($profile){
-            $request -> merge(['customer_id' => $profile -> id]);
-            $data = ProductReview::updateOrCreate(
-                ['customer_id' => $profile -> id, 'product_id' => $request -> input('product_id')],
-                $request -> input()
-            );
-            return ResponseHelper::Out('success', $data, 200);
+        $tran_id = uniqid();
+        $delivery_status = 'Pending';
+        $payment_status = 'Pending';
+
+        $Profile = CustomerProfile::where('user_id', $user_id) -> first();
+        $cus_details = "Name: $Profile->cus_name, Address: $Profile->cus_add, City: $Profile->cus_city, Phone: $Profile->cus_phone";
+        $ship_details = "Name: $Profile->ship_name, Address: $Profile->ship_add, City: $Profile->ship_city, Phone: $Profile->cus_phone";
+
+        // Payable Calculation
+        $total = 0;
+        $cartList = ProductCart::where('user_id', '=', $user_id) -> get();
+        foreach ($cartList as $cartItem){
+            $total = $total + $cartItem -> price;
         }
-        else{
-            return ResponseHelper::Out('fail', 'Customer Profile Not Exists', 200);
-        }
-    }
 
-    public function ProductWishList(Request $request): JsonResponse{
-        $user_id = $request -> header('id');
-        $data = ProductWish::where('user_id', $user_id) -> with('product') -> get();
-        return ResponseHelper::Out('success', $data, 200);
-    }
+        $vat = ($total * 3) / 100;
+        $payable = $total + $vat;
 
-    public function CreateWishList(Request $request): JsonResponse{
-        $user_id = $request -> header('id');
-        $data = ProductWish::updateOrCreate(
-            ['user_id' => $user_id, 'product_id' => $request -> product_id],
-            ['user_id' => $user_id, 'product_id' => $request -> product_id],
-        );
-        return ResponseHelper::Out('success', $data, 200);
-    }
+        $invoice = Invoice::create([
+            'total' => $total,
+            'vat' => $vat,
+            'payable' => $payable,
+            'cus_details' => $cus_details,
+            'ship_details' => $ship_details,
+            'tran_id' => $tran_id,
+            'delivery_status' => $delivery_status,
+            'payment_status' => $payment_status,
+            'user_id' => $user_id
+        ]);
 
-    public function RemoveWishList(Request $request): JsonResponse{
-        $user_id = $request -> header('id');
-        $data = ProductWish::where(['user_id' => $user_id, 'product_id' => $request -> product_id]) -> delete();
-        return ResponseHelper::Out('success', $data, 200);
-    }
+        $invoiceID = $invoice -> id;
 
-    public function CreateCartList(Request $request): JsonResponse{
-        $user_id = $request -> header('id');
-        $product_id = $request -> input('product_id');
-        $color = $request -> input('color');
-        $size = $request -> input('size');
-        $qty = $request -> input('qty');
-
-        $UnitPrice = 0;
-
-        $productDetails = Product::where('id', '=', $product_id) -> first();
-        if($productDetails->discount == 1){
-            $UnitPrice = $productDetails -> discount_price;
-        }
-        else{
-            $UnitPrice = $productDetails -> price;
-        }
-        $totalPrice = $qty * $UnitPrice;
-
-        $data = ProductCart::updateOrCreate(
-            ['user_id' => $user_id, 'product_id' => $product_id],
-            [
+        foreach ($cartList as $EachProduct){
+            InvoiceProduct::create([
+                'invoice_id' => $invoiceID,
+                'product_id' => $EachProduct['product_id'],
                 'user_id' => $user_id,
-                'product_id' => $product_id,
-                'color' => $color,
-                'size' => $size,
-                'qty' => $qty,
-                'price' => $totalPrice,
-            ]
-        );
-        return ResponseHelper::Out('success', $data, 200);
-    }
-    
-    public function CartList(Request $request): JsonResponse{
-        $user_id = $request -> header('id');
-        $data = ProductCart::where('user_id', $user_id) -> with('product') -> get();
-        return ResponseHelper::Out('success', $data, 200);
-    }
+                'qty' => $EachProduct['qty'],
+                'sale_price' => $EachProduct['[price]'],
+            ]);
+        }
 
-    public function DeleteCartList(Request $request): JsonResponse{
-        $user_id = $request -> header('id');
-        $data = ProductCart::where('user_id','=', $user_id) -> where('product_id', '=', $request -> product_id) -> delete();
-        return ResponseHelper::Out('success', $data, 200);
+        
+        
     }
 }
