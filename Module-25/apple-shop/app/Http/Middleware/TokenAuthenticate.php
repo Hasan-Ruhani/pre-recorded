2976dca8e2c6bb1410a7fb6b346d0591b462e3fb
H A S A N <hasan25042019@gmail.com>
2023-09-29 21:26:15 +0600

    sedg

--- Module-25/apple-shop/app/Http/Middleware/TokenAuthenticate.php
+++ Module-25/apple-shop/app/Http/Middleware/TokenAuthenticate.php
@@ -2,10 +2,10 @@
 
     namespace App\Http\Middleware;
 
-use App\Helper\JWTToken;
-use Closure;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
+    use App\Helper\JWTToken;
+    use Closure;
+    use Illuminate\Http\Request;
+    use Symfony\Component\HttpFoundation\Response;
 
     class TokenAuthenticate{
         public function handle(Request $request, Closure $next): Response{
