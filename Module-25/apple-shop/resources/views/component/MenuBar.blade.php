4336bf2fd86ab4414f38991f74d198901e40fa00
H A S A N <hasan25042019@gmail.com>
2023-10-17 22:01:33 +0600

    Update MenuBar.blade.php
    df

--- Module-25/apple-shop/resources/views/component/MenuBar.blade.php
+++ Module-25/apple-shop/resources/views/component/MenuBar.blade.php
@@ -52,7 +52,7 @@
                             </div>
                         </li>
                         <li><a class="nav-link nav_item" href="{{url("/wish")}}"><i class="ti-heart"></i> Wish</a></li>
-                        <li><a class="nav-link nav_item" href="{{url("/")}}"><i class="linearicons-cart"></i> Cart </a></li>
+                        <li><a class="nav-link nav_item" href="{{url("/cart")}}"><i class="linearicons-cart"></i> Cart </a></li>
                         <li><a href="javascript:void(0);" class="nav-link search_trigger"><i class="linearicons-magnifier"></i> Search</a>
                             <div class="search_wrap">
                                 <span class="close-search"><i class="ion-ios-close-empty"></i></span>
